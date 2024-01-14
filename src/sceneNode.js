d9b57c2af8b67a1a5f4cc03fa1de7bd453070c2d
cemborakaya <118216576+cemborakaya@users.noreply.github.com>
2024-01-15 01:14:41 +0300

    Add files via upload

---
+++ src/sceneNode.js
@@ -0,0 +1,46 @@
+/**
+ * @class SceneNode
+ * @desc A SceneNode is a node in the scene graph.
+ * @property {MeshDrawer} meshDrawer - The MeshDrawer object to draw
+ * @property {TRS} trs - The TRS object to transform the MeshDrawer
+ * @property {SceneNode} parent - The parent node
+ * @property {Array} children - The children nodes
+ */
+
+class SceneNode {
+    constructor(meshDrawer, trs, parent = null) {
+        this.meshDrawer = meshDrawer;
+        this.trs = trs;
+        this.parent = parent;
+        this.children = [];
+
+        if (parent) {
+            this.parent.__addChild(this);
+        }
+    }
+
+    __addChild(node) {
+        this.children.push(node);
+    }
+
+    draw(mvp, modelView, normalMatrix, modelMatrix) {
+        /**
+         * @Task1 : Implement the draw function for the SceneNode class.
+         */
+        
+        var transformedMvp = MatrixMult(mvp, this.trs.getTransformationMatrix());
+        var transformedModelView = MatrixMult(modelView, this.trs.getTransformationMatrix());
+        var transformedNormals = MatrixMult(normalMatrix, this.trs.getTransformationMatrix());
+        var transformedModel = MatrixMult(modelMatrix, this.trs.getTransformationMatrix());
+
+        // Draw the MeshDrawer
+        if (this.meshDrawer) {
+            this.meshDrawer.draw(transformedMvp, transformedModelView, transformedNormals, transformedModel);
+        }
+
+        var sphere; //just a variable to go through all the objects in our solar system  
+        for(sphere of this.children) {
+            sphere.draw(transformedMvp, transformedModelView, transformedNormals, transformedModel);
+        }
+    }
+}
